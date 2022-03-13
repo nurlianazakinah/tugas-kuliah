edc380bc0b5e19239b6bca6139d0a0b9c5f50743
nurlianazakinah <101275208+nurlianazakinah@users.noreply.github.com>
2022-03-14 02:28:39 +0800

    Add files via upload

---
+++ tipe data dan operator.py
@@ -0,0 +1,66 @@
+#print data di colab
+
+#tipe data string
+nama="eto"
+print("nama saya:",nama)
+print(type(nama))
+
+#tipe data integer
+umur = 19
+print("umur saya :",umur)
+print(type(umur))
+
+#tipe data float
+tinggi = 1,47
+print("tinggi saya:",tinggi)
+print(type(tinggi))
+
+#tipe data boolean
+saya_pendek = True
+if saya_pendek ==True:
+   print("betul saya pendek")
+
+
+#operator aritmatika
+a = 17
+b = 3
+
+print("hasil+ :", a + b) 
+print("hasil- :", a - b) 
+print("hasil' :", a * b)
+print("hasil/ :", a / b) 
+print("hasil% :", a % b) 
+print("hasil** :", a ** b) 
+print("hasil// :", a // b)
+
+
+
+#operator penugasan dipython
+
+a=10
+print(a)
+
+a+=3
+print(a)
+
+#operator penugasan kurang
+a-=5
+print(a)
+
+#operator penugasan kali
+a*=3
+print(a)
+
+#operator penugass bagi
+a/=4
+print(a)
+
+a%=4
+print(a)
+
+a**=3
+print(a)
+
+#operator penugasan pembagian bilangan bulat
+a//=2
+print(a)
